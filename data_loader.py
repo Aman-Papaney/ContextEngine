ced4b6e1adba7b4ae956164af7ab041c0548d673
Aman-Papaney <amanpapaney2003@gmail.com>
2026-01-12 23:35:56 +0530

    initial commit

---
+++ data_loader.py
@@ -0,0 +1,30 @@
+import os
+from llama_index.readers.file import PDFReader
+from llama_index.core.node_parser import SentenceSplitter 
+from dotenv import load_dotenv
+load_dotenv()
+
+from google import genai
+from google.genai import types
+
+client = genai.Client(api_key = os.getenv("GEMINI_API_KEY"))
+
+EMBED_DIM = 1536
+
+splitter = SentenceSplitter(chunk_size=1000, chunk_overlap = 200)
+
+def load_and_chunk_pdf(path: str):
+    docs = PDFReader().load_data(file = path)
+    texts = [d.text for d in docs if getattr(d, "text", None)]
+    chunks = []
+    for t in texts:
+        chunks.extend(splitter.split_text(t))
+    return chunks
+    
+def embed_texts(texts:list[str]):
+    result = client.models.embed_content(
+        model="gemini-embedding-001",
+        contents= texts,
+        config=types.EmbedContentConfig(output_dimensionality=1536))
+
+    return [embedding.values for embedding in result.embeddings]
---
+++ vector_db.py
@@ -0,0 +1,46 @@
+from qdrant_client import QdrantClient
+from qdrant_client.models import VectorParams, Distance, PointStruct
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+class QdrantStorage:
+    def __init__(self, 
+        url = os.getenv("QDRANT_CLUSTER_ENDPOINT", ""), 
+        api_key = os.getenv("QDRANT_API_KEY", ""),
+        collection = "docs",
+        dim = 1536
+    ):
+        self.client = QdrantClient(url = url, api_key = api_key, timeout = 30)
+        self.collection = collection
+        if not self.client.collection_exists(self.collection):
+            self.client.create_collection(
+                collection_name = self.collection,
+                vectors_config=VectorParams(size = dim, distance = Distance.COSINE)
+            )
+    def upsert(self, ids, vectors, payloads):
+        points = [PointStruct(id = ids[i], vector = vectors[i], payload=payloads[i]) for i in range(len(ids))]
+        self.client.upsert(self.collection, points = points)
+    
+    def search(self, query_vector, top_k: int = 5):
+        results = self.client.query_points(
+            collection_name = self.collection,
+            query = query_vector,
+            with_payload = True,
+            limit = top_k
+        )
+        contexts = []
+        sources = set()
+        
+        for r in results:
+            # print(f"-------results------{r}")
+            payload = r[1][0].payload
+            # print(f"-------payload------{payload}")
+            # print(f"-------payload type------{type(payload)}")
+            text = payload.get("text","")
+            source = payload.get("source","")
+            if text:
+                contexts.append(text)
+                sources.add(source) 
+        # print(f'--------"contexts":{contexts}, "sources":{list(sources)}')
+        return {"contexts":contexts, "sources":list(sources)}
